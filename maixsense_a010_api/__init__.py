6a0bc8a2687ccf05fdfd324d796f72c7a2eb6cbc
Zachary Danzig <82167803+Zinxiee@users.noreply.github.com>
2026-04-27 10:13:46 +0100

    Add files via upload

---
+++ maixsense_a010_api/__init__.py
@@ -0,0 +1,42 @@
+"""Maixsense A010 Python API.
+
+This package provides:
+- USB serial control + streaming frame parsing
+- HTTP frame/config access compatible with the vendor sample scripts
+"""
+
+from .models import (
+    A010UsbFrame,
+    DisplayMode,
+    FrameConfig,
+    HttpFrame,
+    LensCoefficients,
+)
+from .monty_adapter import (
+    CameraIntrinsics,
+    MaixsenseMontyObservationAdapter,
+    create_adapter_from_http_calibration,
+)
+from .usb_client import MaixsenseA010USB
+from .usb_frame_client import (
+    A010UsbFrameClient,
+    SensorConnectionError,
+    SensorTimeoutError,
+)
+from .http_client import MaixsenseA010HTTP
+
+__all__ = [
+    "A010UsbFrame",
+    "DisplayMode",
+    "FrameConfig",
+    "HttpFrame",
+    "LensCoefficients",
+    "CameraIntrinsics",
+    "MaixsenseMontyObservationAdapter",
+    "create_adapter_from_http_calibration",
+    "MaixsenseA010USB",
+    "A010UsbFrameClient",
+    "SensorConnectionError",
+    "SensorTimeoutError",
+    "MaixsenseA010HTTP",
+]
